1491109867aa1d7a3ccb963df4232900c73d2765
berk  başaran <berkbasaran@berk-MacBook-Pro.local>
2024-08-28 22:10:10 +0300

    first commit

---
+++ Day 1/final_project.py
@@ -0,0 +1,6 @@
+print("Welccome to the Band Generator.")
+city = input("Which city did you grow up in?\n")
+
+pet = input("What is the name of pet?\n")
+
+print("Your band name could be: "+ city + " " + pet)
---
+++ Day 1/helloworld.py
@@ -0,0 +1,31 @@
+print("hello world!")
+# Strings : Written in double quote. 
+# Underline üzerine gelince hatayı gösteriyor.
+
+# \n yeni satıra indiriyor
+print("hello world! \nhello world Berk!")
+print("hello" + "Alp ve Berk")
+print("Hello" + " " + "Alp ve Berk")
+
+# paratez öncesinde boşluk olmayacak!
+# hataları oku!
+
+# get the input of the user - identical to print function
+# input("What is your name?")
+# input edilen girdiyi saklaman gerekiyor String olarak
+# print içerisine alınan input u önce soruyor sonra print işlemini yapıyor.
+
+print("Hello " +input("What is your name?") + "!")
+
+# Variable atama
+name = "Jack" 
+print(name)
+name = "Angela"
+print(name)
+print(len(input("What is your age?")))
+# len() karakter sayıyor.
+
+username = input("What is your name?")
+length = len(username)
+print(length)
+
