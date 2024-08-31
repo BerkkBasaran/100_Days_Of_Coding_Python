ad60a422579aacae5d23768629a1a98ffc756d80
berk  başaran <berkbasaran@berk-MacBook-Pro.local>
2024-08-31 16:47:42 +0300

    Day 6 is added.

---
+++ Day 6/while_loop.py
@@ -0,0 +1,28 @@
+# functions check out website for all functions
+# print() --> output function
+# len() --> calculates characters
+# making own function by def my_function():
+
+#def my_function():
+#    print("hello")
+#    print("bye")
+
+#my_function() # calling function
+
+#def turn_right():
+#    turn_left()
+#    turn_left()
+#    turn_left()
+
+#turn_left
+
+
+#while loop continue when condition is true, when false ends.
+# while condition_test
+#   do this
+#   get this
+
+# while at_goal() != True: --> while not at_goal():
+# while not can be used when first condition is false, final condition is true. when the condition needs to be changed.
+# for da sınırı belirleyebiliyorsun ama while da condition dışına çıkana kadar devam ediyor.
+# infinite loop tehlikesi var!!!
