e4a962084c44ed155e8b96eddecbd462782c250f
berk  başaran <berkbasaran@berk-MacBook-Pro.local>
2024-09-06 21:47:09 +0300

    Day 11 is added.

---
+++ day_11/day_11_notes.py
@@ -0,0 +1,7 @@
+import random
+
+mylist = ["apple", "banana", "cherry"]
+
+print(random.choices(mylist, weights = [10, 1, 1], k = 14))
+print(len(random.choices(mylist, weights = [10, 1, 1], k = 14)))
+
